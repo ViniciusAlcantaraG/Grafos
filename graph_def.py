08687044df677ad6c5b02acdea0598b3efa2a8bd
rickauer <116601341+rickauer@users.noreply.github.com>
2023-09-21 21:10:33 -0300

    Update graph_def.py

--- graph_def.py
+++ graph_def.py
@@ -1,3 +1,20 @@
+import numpy as np
+import functools 
+import statistics 
+from time import time
+import random
+
+with open("grafo_1.txt") as f:
+    mylist = f.read().splitlines() 
+links = []
+for i in range(1, len(mylist)):
+    a,b = mylist[i].split(" ")
+    links.append([int(a),int(b)])
+number_vertices = int(mylist[0])
+print(number_vertices, len(links))
+
+
+
 class Grafo:
     def __init__(self, n, links):
         self.n = n
@@ -63,7 +80,7 @@ class Stack:
 class Matriz_Grafo(Grafo):
     def __init__(self, n, links):
         super().__init__(n, links)
-        self.matriz = [[0 for j in range(self.n)] for i in range(self.n)]
+        self.matriz = np.zeros((self.n, self.n), dtype = int) 
         for i in range(len(links)):
             a = links[i][0] - 1
             b = links[i][1] - 1
@@ -73,28 +90,27 @@ class Matriz_Grafo(Grafo):
         return str(self.matriz)
     
     def BFS(self, raiz):
-        nivel = [0 for i in range(self.n)]
-        pai = [0 for i in range(self.n)]
-        self.verticesBFS = [0 for i in range(self.n)]
-        fila = []
+        nivel = np.zeros(self.n, dtype = int)
+        pai = np.zeros(self.n, dtype = int)
+        self.verticesBFS = np.zeros(self.n, dtype = int)
+        fila = np.array([raiz-1])
         self.verticesBFS[raiz - 1] = 1
-        fila.append(raiz-1)
-        while fila != []:
+        while len(fila) != 0:
             v = fila[0]
-            fila.pop(0)
+            fila = fila[1:]
             for i in range(len(self.matriz[v])):
                 if self.matriz[v][i] == 1:
                     if self.verticesBFS[i] == 0:
                         pai[i] = v + 1
                         nivel[i] = nivel[v] + 1
                         self.verticesBFS[i] = 1
-                        fila.append(i)
+                        fila = np.concatenate((fila, np.array([i])))
         return pai, nivel
     
     def DFS(self, raiz):
-        nivel = [0 for i in range(self.n)]
-        pai = [0 for i in range(self.n)]
-        vertices = [0 for i in range(self.n)]
+        nivel = np.zeros(self.n, dtype = int)
+        pai = np.zeros(self.n, dtype = int)
+        vertices = np.zeros(self.n, dtype = int)
         pilha = Stack()
         pilha.push(raiz-1)
         while pilha.isEmpty() == False:
@@ -113,37 +129,38 @@ class Matriz_Grafo(Grafo):
         return self.BFS(u)[1][v-1]
     
     def diametro(self):
-        distancias = []
+        distancias = np.array([])
         for i in range(1, self.n + 1):
             for j in range(1, self.n + 1):
                 if i!=j:
-                    distancias.append(self.distancia(i,j))
-        return max(distancias)
+                    distancias = np.concatenate((distancias, np.array([self.distancia(i,j)])))
+        return int(distancias.max())
     
     def componentes_conexos(self):
-        tamanho = []
+        tamanho = np.array([], dtype = int)
         Ver_BFS = [0 for i in range(self.n)]
-        lista_vert = []
+        lista_vert = np.array([], dtype = int)
         self.BFS(1)
-        tamanho.append(self.verticesBFS.count(1))
+        tamanho = np.concatenate((tamanho, np.array([np.count_nonzero(self.verticesBFS == 1)])))
         for i in range(self.n):
             if self.verticesBFS[i] == 1:
-                lista_vert.append(i+1)
+                lista_vert = np.concatenate((lista_vert,np.array([i+1])))
                 Ver_BFS[i] = 1
         while len(lista_vert) != self.n:
             if Ver_BFS.index(0)+1 not in lista_vert:
                 self.BFS(Ver_BFS.index(0)+1)
-                tamanho.append(self.verticesBFS.count(1))
+                tamanho = np.concatenate((tamanho, np.array([np.count_nonzero(self.verticesBFS == 1)])))
                 for i in range(self.n):
                     if self.verticesBFS[i] == 1:
-                        lista_vert.append(i+1)
+                        lista_vert = np.concatenate((lista_vert, np.array([i+1])))
                         Ver_BFS[i] = 1
-        x = 0
-        lista2 = []
+        lista_vert = list(lista_vert)
         for i in range(len(tamanho)):
-            lista2.append(lista_vert[x:tamanho[i]+x])
-            x = tamanho[i]
-        return tamanho, lista2, len(tamanho)
+            if i != len(tamanho) - 1:
+                lista_vert[i:tamanho[i]] = [lista_vert[i:tamanho[i]]]
+            else:
+                lista_vert[i:] = [lista_vert[i:]]
+        return tamanho, lista_vert, len(tamanho)
     
 class Lista_Grafo(Grafo):
     def __init__(self, n, links):
@@ -159,27 +176,26 @@ class Lista_Grafo(Grafo):
         return str(self.lista)
     
     def BFS(self, raiz):
-        nivel = [0 for i in range(self.n)]
-        pai = [0 for i in range(self.n)]
-        self.vertices_BFS = [0 for i in range(self.n)]
-        fila = []
+        nivel = np.zeros(self.n, dtype = int)
+        pai = np.zeros(self.n, dtype = int)
+        self.vertices_BFS = np.zeros(self.n, dtype = int)
+        fila = np.array([raiz-1])
         self.vertices_BFS[raiz-1] = 1
-        fila.append(raiz-1)
-        while fila != []:
+        while len(fila) != 0:
             v = fila[0]
-            fila.pop(0)
+            fila = fila[1:]
             for i in self.lista[v]:
                 if self.vertices_BFS[i-1] == 0:
                     pai[i-1] = v + 1
                     nivel[i-1] = nivel[v] + 1
                     self.vertices_BFS[i-1] = 1
-                    fila.append(i-1)
+                    fila = np.concatenate((fila, np.array([i-1])))
         return pai, nivel
     
     def DFS(self, raiz):
-        nivel = [0 for i in range(self.n)]
-        pai = [0 for i in range(self.n)]
-        vertices = [0 for i in range(self.n)]
+        nivel = np.zeros(self.n, dtype = int)
+        pai = np.zeros(self.n, dtype = int)
+        vertices = np.zeros(self.n, dtype = int)
         pilha = Stack()
         pilha.push(raiz-1)
         while pilha.isEmpty() == False:
@@ -197,33 +213,34 @@ class Lista_Grafo(Grafo):
         return self.BFS(u)[1][v-1]
     
     def diametro(self):
-        distancias = []
+        distancias = np.array([])
         for i in range(1, self.n + 1):
             for j in range(1, self.n + 1):
                 if j <=i:
                     pass
                 else:
-                    distancias.append(self.distancia(i,j))
-        return max(distancias)
+                    distancias = np.concatenate((distancias, np.array([self.distancia(i,j)])))
+        return int(distancias.max())
     
     def componentes_conexos(self):
-        tamanho = []
+        tamanho = np.array([], dtype = int)
         Ver_BFS = [0 for i in range(self.n)]
-        lista_vert = []
+        lista_vert = np.array([], dtype = int)
         self.BFS(1)
-        tamanho.append(self.vertices_BFS.count(1))
+        tamanho = np.concatenate((tamanho, np.array([np.count_nonzero(self.vertices_BFS == 1)])))
         for i in range(self.n):
             if self.vertices_BFS[i] == 1:
-                lista_vert.append(i+1)
+                lista_vert = np.concatenate((lista_vert,np.array([i+1])))
                 Ver_BFS[i] = 1
         while len(lista_vert) != self.n:
             if Ver_BFS.index(0)+1 not in lista_vert:
                 self.BFS(Ver_BFS.index(0)+1)
-                tamanho.append(self.vertices_BFS.count(1))
+                tamanho = np.concatenate((tamanho, np.array([np.count_nonzero(self.vertices_BFS == 1)])))
                 for i in range(self.n):
                     if self.vertices_BFS[i] == 1:
-                        lista_vert.append(i+1)
+                        lista_vert = np.concatenate((lista_vert, np.array([i+1])))
                         Ver_BFS[i] = 1
+        lista_vert = list(lista_vert)
         for i in range(len(tamanho)):
             if i != len(tamanho) - 1:
                 lista_vert[i:tamanho[i]] = [lista_vert[i:tamanho[i]]]
@@ -232,5 +249,27 @@ class Lista_Grafo(Grafo):
         return tamanho, lista_vert, len(tamanho)
 
 
-cachorro = Lista_Grafo(5, [[1,2],[2,5],[5,3],[4,5],[1,5]])
-print(cachorro)
+cachorrinho = Lista_Grafo(5, [[1,2], [2,3], [1,3], [4,5]])
+print(cachorrinho.componentes_conexos())
+cachorro = Lista_Grafo(number_vertices, links)
+a=[[1,2,3],[2],[1,2]]
+b = np.array(cachorro.lista)
+b = np.array(list(map(len, b)))
+#media = functools.reduce(lambda a, c: a+c, b)//10000
+media = statistics.mean(b)
+mediana = statistics.median(np.sort(b))
+tamanho = cachorro.componentes_conexos()[2]
+tamanho_de_cada_componente = cachorro.componentes_conexos()[0]
+vertices_conexos = cachorro.componentes_conexos()[1]
+
+g = open("saida.txt", "w")
+g.write("Número de vértices: " + str(number_vertices) + "\n" + 
+        "Número de arestas: " + str(len(links)) + "\n" +
+        "Grau mínimo: " + str(b.min()) + "\n" + 
+        "Grau máximo: " + str(b.max()) + "\n" + 
+        "Grau médio: " + str(media) + "\n" +
+        "Mediana de grau: " + str(int(mediana)) + "\n" +
+        "Número de componentes conexas: " + str(tamanho) + "\n" +
+        "Tamanho de cada componente: " + str(tamanho_de_cada_componente) + "\n" + 
+        "Lista de vértices pertencentes à componente: " + str(vertices_conexos))
+tempos = []
